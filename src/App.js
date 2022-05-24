114103985a5875694c402dd8bc0ffbc8d2025700
John-Ray <johnrayhendricks103@gmail.com>
2022-05-25 01:14:28 +0200

    Added Navbar component with styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+    </>
+  );
 }
 
 export default App;
