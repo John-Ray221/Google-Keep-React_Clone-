32bbac95e2bc37a9e630b7137c2ae07fa8d446e8
John-Ray <johnrayhendricks103@gmail.com>
2022-05-25 12:22:54 +0200

    Components: Added Form,Notes and Modal components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Form from "./components/Form/Form";
+import Notes from "./components/Notes/Notes";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
     <>
       <Navbar />
       <Sidebar />
+      <Form />
+      <Notes />
+      <Modal />
     </>
   );
 }
---
+++ src/components/Notes/Notes.js
@@ -0,0 +1,200 @@
+import "./Notes.css";
+
+export default function Notes() {
+  return (
+    <div className="notes">
+      <div
+        className="note"
+        id="note.id"
+        // onmouseover=""
+        // onmouseout=""
+      >
+        <span className="material-icons check-circle">check_circle</span>
+        <div className="title">TITLE</div>
+        <div className="text">TEXT</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              add_alert
+            </span>
+            <span className="tooltip-text">Remind me</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              person_add
+            </span>
+            <span className="tooltip-text">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              palette
+            </span>
+            <span className="tooltip-text">Change Color</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              image
+            </span>
+            <span className="tooltip-text">Add Image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              archive
+            </span>
+            <span className="tooltip-text">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              more_vert
+            </span>
+            <span className="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+      <div
+        className="note"
+        id="note.id"
+        // onmouseover=""
+        // onmouseout=""
+      >
+        <span className="material-icons check-circle">check_circle</span>
+        <div className="title">TITLE</div>
+        <div className="text">TEXT</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              add_alert
+            </span>
+            <span className="tooltip-text">Remind me</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              person_add
+            </span>
+            <span className="tooltip-text">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              palette
+            </span>
+            <span className="tooltip-text">Change Color</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              image
+            </span>
+            <span className="tooltip-text">Add Image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              archive
+            </span>
+            <span className="tooltip-text">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              more_vert
+            </span>
+            <span className="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+      <div
+        className="note"
+        id="note.id"
+        // onmouseover=""
+        // onmouseout=""
+      >
+        <span className="material-icons check-circle">check_circle</span>
+        <div className="title">TITLE</div>
+        <div className="text">TEXT</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              add_alert
+            </span>
+            <span className="tooltip-text">Remind me</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              person_add
+            </span>
+            <span className="tooltip-text">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              palette
+            </span>
+            <span className="tooltip-text">Change Color</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              image
+            </span>
+            <span className="tooltip-text">Add Image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              archive
+            </span>
+            <span className="tooltip-text">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              more_vert
+            </span>
+            <span className="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+      <div
+        className="note"
+        id="note.id"
+        // onmouseover=""
+        // onmouseout=""
+      >
+        <span className="material-icons check-circle">check_circle</span>
+        <div className="title">TITLE</div>
+        <div className="text">TEXT</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              add_alert
+            </span>
+            <span className="tooltip-text">Remind me</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              person_add
+            </span>
+            <span className="tooltip-text">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              palette
+            </span>
+            <span className="tooltip-text">Change Color</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              image
+            </span>
+            <span className="tooltip-text">Add Image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              archive
+            </span>
+            <span className="tooltip-text">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-icons-outlined hover small-icon">
+              more_vert
+            </span>
+            <span className="tooltip-text">More</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
